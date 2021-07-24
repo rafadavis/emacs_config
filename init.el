179705f997873b7f959e7faa77128058704b8f46
rafadavis <rdavisportela@gradcenter.cuny.edu>
2021-07-24 12:39:03 -0400

    regular init.el commit

--- init.el
+++ init.el
@@ -22,10 +22,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(wheatgrass))
- '(org-capture-templates
-   '(("i" "Inbox: to be processed" entry
-      (file+headline "" "inbox")
-      "* %u"))))
+ '(package-selected-packages '(auctex)))
+ 
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -37,25 +35,29 @@
 (add-to-list 'default-frame-alist '(font . "Courier New 18"))
 (set-face-attribute 'default t :font "Courier New 18") ;; setting default font and font size
 (add-to-list 'default-frame-alist '(fullscreen . maximized)) ;; to open in maximized window
-
-
+(tool-bar-mode -1) ;; gets rid of tool bar. The one with icons for new, copy, paste, etc
+(toggle-scroll-bar -1) ;; gets rid of scroll bar, only usefull with a mouse, and mouse scroll still works without it
+;;(menu-bar-mode -1) ;; I like the menu bar, it is not large and there's a lot there that I have no idea that exists. But this code is here if I wanna take it of. I can also run any of those with M-:
 
 ;; org settings
 
 (setq org-agenda-files '("c:/Users/rdpor/Dropbox/org/projects.org"))
 
-(setq org-agenda-custom-commands
-      '(("@" . "HOME + first letter for context") ; describe prefix "@"
-        ("@e" tags "@errands")
-        ("@h" tags "@home")
-        ("@p" tags "@pc")))
+(setq org-agenda-span 1)
+;; (setq org-agenda-custom-commands
+;;       '(("@" . "HOME + first letter for context") ; describe prefix "@"
+;;         ("@e" tags "@errands")
+;;         ("@h" tags "@home")
+;;         ("@p" tags "@pc")))
 ;; adding choice of searching specific tags in agenda
+;; commented out, because the 'm' agenda option solves this for me
+;; I'll leave it here for a bit, in case the code would still be useful for other stuff
 
 (setq org-agenda-dim-blocked-tasks 'invisible)
 ;; removing from agenda projects that are blocked (by subtodos or something)
 ;; set to t if want to dim instead of disappear
 
-(setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w!)" "|" "CANCELLED(c!)" "DONE(d!)")))
+(setq org-todo-keywords '((sequence "TODO(t!)" "WAITING(w@)" "CANCELLED(c@)" "DONE(d!)")))
 ;; setting todo to have waiting and cancelled options
 ;; (setq org-log-done t)
 ;; To log done todos
@@ -63,12 +65,26 @@
 
 (setq org-stuck-projects '("+LEVEL=1/-DONE-CANCELLED" ("TODO" "WAITING" "") nil ""))
 
+(setq org-priority-faces '((?A . (:foreground "red" :weight 'bold))
+                           (?B . (:foreground "yellow"))
+                           (?C . (:foreground "green"))))
+;; gives different colors for each priority
+
 (setq org-default-notes-file "c:/Users/rdpor/Dropbox/org/projects.org")
 
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cc" 'org-capture)
 
+(setq org-capture-templates
+   '(("i" "Inbox: to be processed" entry
+      (file "")
+      "* captured on %u \n %?")))
+;; template for inbox
+;; will drop  in default-notes-file ("")
+;; * and text to make it a heading
+;; %u for timestamp that doesn't go to agenda
+;; \n newline and %? for more input
 
 (setq org-log-into-drawer t)
 ;; so the todo state logs go to a drawer instead of the body of the text
@@ -84,6 +100,9 @@
 			   ("c:/Users/rdpor/Dropbox/org/i.org" :maxlevel . 1)))
 ;; setting targets for refile. Need to better understand the maxlevel
 
+(put 'narrow-to-region 'disabled nil) ;; option to narrow to region C-x n n
+;; it is disabled by default
+
 (setq org-agenda-show-all-dates nil)
 ;; in the agend show only dates that have things schedule
 
@@ -92,7 +111,9 @@
 
 (global-visual-line-mode 1)
  ;; This allows for a soft line break that does not break words in the middle
- ;; 1 for on, 0 for off.
+;; 1 for on, 0 for off.
+
+(add-hook 'prog-mode-hook 'display-line-numbers-mode) ;; automatically show line numbers in all programming modes.
 
 (setq visible-bell 1)
 ;; removes beep and change it to a light when in the end of file
@@ -132,7 +153,11 @@
 (prefer-coding-system 'utf-8)
 ;; defining the default encoding system
 
-(put 'upcase-region 'disabled nil)
+(defun prev-window ()
+  (interactive)
+  (other-window -1)) ;; function to go to previouse window
+(global-set-key (kbd "C-,") #'prev-window) ;; set keybind to go to previous window
+(global-set-key (kbd "C-.") #'other-window) ;; change other-window keybind to make it easier and more logic
 
 
 (defun jump-to-same-spot-in-other-buffer () 
@@ -144,13 +169,30 @@
        (when (invisible-p (point))
         (org-reveal)))
 )
-;; function to go from index on one side to content in the other
 
 (global-set-key (kbd "C-x j") 'jump-to-same-spot-in-other-buffer)
+;; function to go from index on one side to content in the other
 
+(defun org-narrow-to-subtree-in-cloned-window (newname display-flag &optional norecord)
+  "Open a clone in a new window and narrow to subtree"
+  (interactive
+   (progn
+     (if (get major-mode 'no-clone-indirect)
+	 (error "Cannot indirectly clone a buffer in %s mode" mode-name))
+     (list (if current-prefix-arg
+	       (read-buffer "Name of indirect buffer: " (current-buffer)))
+	   t)))
+  (let ((pop-up-windows t))
+    (clone-indirect-buffer-other-window newname display-flag norecord))
+  ;;; the above I copied from clone-indirect-buffer-other-window, which had the same code, but ended calling (clone-indirect-buffer), which is based on
+  (prev-window)
+  (jump-to-same-spot-in-other-buffer)
+  (org-narrow-to-subtree)
+  )
+(global-set-key (kbd "C-x n c") 'org-narrow-to-subtree-in-cloned-window)
 
-
-
+(setq org-startup-indented t) ;; so all files will have indented hierarchical headlines
+;;(setq org-indent-mode-turns-on-hiding-stars nil) ;; if I want to show all stars in headings
 
 ;; below: stuff about reading text accessibility, blind vision things
 (defun play-audio-input (input-to-say) "Plays input as speech." (shell-command
@@ -200,3 +242,12 @@
 ;; setting register 'i' to my init file
 ;; to quickly access it, C-x r j i
 (set-register ?p '(file . "c:/Users/rdpor/Dropbox/org/projects.org"))
+
+(set-register ?m '(file . "c:/Users/rdpor/Dropbox/phd/main.org"))
+;; latex stuff below from this guide https://www.latexbuch.de/files/latexsystem-en.pdf
+;;(server-start) need to read more about this and see if it's needed.
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex);; so it activates reftex automatically in AUCTex LaTeX mode
+(add-hook 'latex-mode-hook 'turn-on-reftex) ;; so it activates reftex automatically in Emacs latex mode
+(setq reftex-plug-into-AUCTeX t) ;; integrates RefTeX with AUCTeX
+;; (setq-default ispell-program-name "aspell") ;; aspell for windows no longer maintained
+;; havent' installed it. Other options for windows not looking great, need to ask other people.
